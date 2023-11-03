6773b02b9ac0b400fa34c3d3691f40deb21a38bb
BenQUUU <michal.kolczak123@gmail.com>
2023-11-03 19:21:11 +0100

    Added insertion of element into board

--- Board.py
+++ Board.py
@@ -1,3 +1,4 @@
+from random import randint
 from Element import Element
 
 
@@ -9,3 +10,14 @@ class Board:
             for y in range(0, 4):
                 self.board[y].append(Element())
                 self.prevBoard[y].append(Element())
+
+    def randomizeElement(self):
+        #Randomizing a new element
+        x = randint(0, 3)
+        y = randint(0, 3)
+
+        while self.board[y][x].getVal() != 0:
+            x = randint(0, 3)
+            y = randint(0, 3)
+
+        self.board[y][x].setVal(randint(1, 2) * 2)
